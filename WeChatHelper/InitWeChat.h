e02dd260391d1df14893b7e84ef5e7d441f02127
A <49177898+bbgouzi123@users.noreply.github.com>
2026-09-17 21:10:54 +0800

    Clear repository contents

--- WeChatHelper/InitWeChat.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "stdafx.h"
-
-BOOL IsWxVersionValid();	//鵱ǰ΢Ű汾
-void CheckIsLogin();	//΢Ƿ½
-UINT64 GetAccountService();	//获取账户服务管理器 (3.9版本)
--- WeChatRobot/CInjectTools.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-DWORD ProcessNameFindPID(const char* ProcessName);	//通过进程名获取进程ID
-BOOL InjectDll(HANDLE& wxPid); //注入dll
-BOOL CheckIsInject(DWORD dwProcessid);	//检测DLL是否已经注入
